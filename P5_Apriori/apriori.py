7b072824e5c28cdae0b9b6c6f423b853f417bdf1
claudiadmr <claudiadm.ribeiro@hotmail.com>
2023-05-28 18:35:39 +0100

    updated unit test in apriori

--- P5_Apriori/apriori.py
+++ P5_Apriori/apriori.py
@@ -1,4 +1,4 @@
-from typing import List, Set, Dict
+from typing import List, Set
 
 class TransactionDataset:
     def __init__(self, transactions: List[Set[int]]):
--- P5_Apriori/test.py
+++ P5_Apriori/test.py
@@ -1,13 +1,9 @@
 import unittest
-from os import name
-
-# Import the classes to be tested
 from apriori import TransactionDataset, Apriori, AssociationRules
 
 class TestTransactionDataset(unittest.TestCase):
-    def test_frequent_items(self):
-        # Create a TransactionDataset object
-        dataset = TransactionDataset([
+    def setUp(self):
+        self.transactions = [
             {1, 2, 3},
             {1, 2, 3, 4},
             {2, 3, 4},
@@ -16,16 +12,17 @@ class TestTransactionDataset(unittest.TestCase):
             {1, 3, 4},
             {1, 3, 5},
             {1, 2, 3, 4, 5}
-        ])
+        ]
+        self.dataset = TransactionDataset(self.transactions)
 
-        # Test the frequent_items() method
-        expected = [            {3, 2, 1},            {3, 2},            {3, 1},            {3},            {2},            {1}        ]
-        self.assertEqual(dataset.frequent_items(), expected)
+    def test_frequent_items(self):
+        expected = [(3, 8), (1, 6), (2, 6), (4, 4), (5, 4)]
+        self.assertEqual(self.dataset.freq_items, expected)
+
 
 class TestApriori(unittest.TestCase):
-    def test_freq_itemsets(self):
-        # Create a TransactionDataset object
-        dataset = TransactionDataset([
+    def setUp(self):
+        self.transactions = [
             {1, 2, 3},
             {1, 2, 3, 4},
             {2, 3, 4},
@@ -34,34 +31,30 @@ class TestApriori(unittest.TestCase):
             {1, 3, 4},
             {1, 3, 5},
             {1, 2, 3, 4, 5}
-        ])
+        ]
+        self.dataset = TransactionDataset(self.transactions)
+        self.apriori = Apriori(self.dataset, min_support=0.5)
 
-        # Create an Apriori object
-        apriori = Apriori(dataset, min_support=0.5)
-
-        # Test the freq_itemsets attribute
+    def test_freq_itemsets(self):
         expected = {
-            frozenset({3}): 7,
+            frozenset({3}): 8,
+            frozenset({1}): 6,
             frozenset({2}): 6,
-            frozenset({1}): 5,
-            frozenset({2, 3}): 5,
+            frozenset({4}): 4,
+            frozenset({5}): 4,
+            frozenset({2, 3}): 6,
             frozenset({1, 2}): 4,
-            frozenset({1, 3}): 4,
-            frozenset({3, 5}): 3,
-            frozenset({1, 2, 3}): 3,
-            frozenset({4}): 3,
-            frozenset({1, 5}): 2,
-            frozenset({2, 4}): 2,
-            frozenset({3, 4}): 2,
-            frozenset({1, 2, 5}): 2,
-            frozenset({2, 3, 5}): 2
+            frozenset({1, 3}): 6,
+            frozenset({3, 4}): 4,
+            frozenset({3, 5}): 4,
+            frozenset({1, 2, 3}): 4
         }
-        self.assertEqual(apriori.freq_itemsets, expected)
+        self.assertEqual(self.apriori.freq_itemsets, expected)
+
 
 class TestAssociationRules(unittest.TestCase):
-    def test_rules(self):
-        # Create a TransactionDataset object
-        dataset = TransactionDataset([
+    def setUp(self):
+        self.transactions = [
             {1, 2, 3},
             {1, 2, 3, 4},
             {2, 3, 4},
@@ -70,19 +63,20 @@ class TestAssociationRules(unittest.TestCase):
             {1, 3, 4},
             {1, 3, 5},
             {1, 2, 3, 4, 5}
-        ])
+        ]
+        self.dataset = TransactionDataset(self.transactions)
+        self.apriori = Apriori(self.dataset, min_support=0.5)
+        self.rules = AssociationRules(self.apriori, min_confidence=0.8)
 
-        # Create an Apriori object
-        apriori = Apriori(dataset, min_support=0.5)
-
-        # Create an AssociationRules object
-        rules = AssociationRules(apriori, min_confidence=0.8)
-
-        # Test the rules() method
-        expected = [        ({3}, {1}, 0.5714285714285714),        ({3}, {2}, 0.7142857142857143),        ({2, 3}, {1}, 0.6),        ({1, 3}, {2}, 1.0),        ({3, 5}, {2}, 0.6666666666666666),        ({2, 3, 5}, {1}, 1.0)    ]
-        for i, rule in enumerate(rules.rules()):
-            self.assertEqual(rule, expected[i])
+    def test_rules(self):
+        expected = [
+            (frozenset({2}), frozenset({3}), 1.0),
+            (frozenset({1}), frozenset({3}), 1.0),
+            (frozenset({4}), frozenset({3}), 1.0),
+            (frozenset({5}), frozenset({3}), 1.0)
+        ]
+        self.assertEqual(self.rules.rules, expected)
 
 
-if name == 'main':
+if __name__ == '__main__':
     unittest.main()
